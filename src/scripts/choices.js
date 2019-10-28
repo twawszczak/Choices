75d8c546d28bf8f89f77d9ae778488c49385ed32
Josh Johnson <josh@joshuajohnson.co.uk>
2019-10-28 18:03:25 +0000

    Add todo

--- src/scripts/choices.js
+++ src/scripts/choices.js
@@ -431,6 +431,7 @@ class Choices {
     }
 
     this.containerOuter.removeLoadingState();
+
     const addGroupsAndChoices = groupOrChoice => {
       if (groupOrChoice.choices) {
         this._addGroup({
